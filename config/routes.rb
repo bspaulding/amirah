f94d61c098fee7a877a96e3e52b202232bf33c4b
Bradley J. Spaulding <brad.spaulding@gmail.com>
2012-08-17 05:32:46 -0400

    Added static pages for global nav.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 AmirahRails::Application.routes.draw do
   resources :articles
 
+  ["who-we-are", "what-we-do", "resources", "contact", "donate"].each do |slug|
+    match "/#{slug}" => "articles##{slug.underscore}"
+  end
+
   root :to => "articles#index"
 end
